5501541072302b0706b06cc1a6f3a5f14db3c783
mklunder <mklunder@stelktech.com>
2021-11-22 06:17:05 -0800

    Changed MCP names to Yarn Naming so Mouse tweaks can run in dev environment (IntelliJ) for debugging. Forge Builds will be broken with this change when running from a Dev environment.
    Made some small dependency versioning changes.
    
    Commented out the gradle Build File Name as the only way I can get
    debugger to work is to replace the contents of build.gradle with
    build-fabric.gradle (on local machine only)

--- src/main/java/yalter/mousetweaks/Constants.java
+++ src/main/java/yalter/mousetweaks/Constants.java
@@ -14,13 +14,13 @@ public class Constants {
 
 	// Names for reflection.
 	public static final ObfuscatedName IGNOREMOUSEUP_NAME
-			= new ObfuscatedName("skipNextRelease", "f_97719_", "field_2798", "Q");
+			= new ObfuscatedName("cancelNextRelease", "f_97719_", "field_2798", "Q");
 	public static final ObfuscatedName DRAGSPLITTING_NAME
-			= new ObfuscatedName("isQuickCrafting", "f_97738_", "field_2794", "y");
+			= new ObfuscatedName("cursorDragging", "f_97738_", "field_2794", "y");
 	public static final ObfuscatedName DRAGSPLITTINGBUTTON_NAME
-			= new ObfuscatedName("quickCraftingButton", "f_97718_", "field_2778", "P");
+			= new ObfuscatedName("heldButtonCode", "f_97718_", "field_2778", "P");
 	public static final ObfuscatedName GETSELECTEDSLOT_NAME
-			= new ObfuscatedName("findSlot", "m_97744_", "method_2386", "a");
+			= new ObfuscatedName("getSlotAt", "m_97744_", "method_2386", "a");
 	static final ObfuscatedName HANDLEMOUSECLICK_NAME
-			= new ObfuscatedName("slotClicked", "m_6597_", "method_2383", "a");
+			= new ObfuscatedName("onMouseClick", "m_6597_", "method_2383", "a");
 }
